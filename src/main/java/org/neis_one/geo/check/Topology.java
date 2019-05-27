2b216ed84c7f341cdebaf1930f3b85e3751765a6
pa5cal <pascal@neis-one.org>
2019-05-27 08:43:56 +0200

    New example for checking Topological Consistency

---
+++ src/main/java/org/neis_one/geo/check/Topology.java
@@ -0,0 +1,13 @@
+package org.neis_one.geo.check;
+
+import org.locationtech.jts.geom.Geometry;
+
+public class Topology {
+
+	/**
+	 * Checks if geometry is valid.
+	 */
+	public static boolean isValid(Geometry geom) {
+		return geom.isValid();
+	}
+}
---
+++ src/main/java/org/neis_one/geo/quality/main/TopologicalConsistency.java
@@ -0,0 +1,45 @@
+package org.neis_one.geo.quality.main;
+
+import java.io.IOException;
+
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.locationtech.jts.geom.Geometry;
+import org.neis_one.geo.check.Topology;
+import org.neis_one.geo.file.shp.ReadShapefile;
+import org.opengis.feature.simple.SimpleFeature;
+
+/**
+ * 
+ * Example: Check Topological Consistency of all shapefile features. (Logical
+ * consistency)
+ * 
+ * @author pa5cal
+ *
+ */
+public class TopologicalConsistency {
+
+	public static void main(String[] args) {
+		// Read Shapefile
+		SimpleFeatureSource lineFeatures = new ReadShapefile().get("./src/test/resources/samplelines/SampleLines.shp");
+
+		// Check topology
+		int validFeatures = 0;
+		int features = 0;
+		try {
+			SimpleFeatureIterator iter = lineFeatures.getFeatures().features();
+			while (iter.hasNext()) {
+				features++;
+				SimpleFeature line = iter.next();
+				validFeatures += Topology.isValid((Geometry) line.getDefaultGeometry()) ? 1 : 0;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Output result
+		System.out.println("RESULT");
+		System.out.println("Features: " + features);
+		System.out.println("Valid: " + validFeatures);
+	}
+}
