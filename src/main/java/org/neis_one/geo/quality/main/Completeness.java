691b8bde810f89d0aa01e408a0d91edcd6ac951b
pa5cal <pascal@neis-one.org>
2019-05-27 11:32:15 +0200

    New positional accuracy example

--- src/main/java/org/neis_one/geo/quality/main/Completeness.java
+++ src/main/java/org/neis_one/geo/quality/main/Completeness.java
@@ -2,13 +2,9 @@ package org.neis_one.geo.quality.main;
 
 import java.io.IOException;
 
-import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
-import org.geotools.geometry.jts.Geometries;
-import org.locationtech.jts.geom.Geometry;
 import org.neis_one.geo.calculation.Length;
 import org.neis_one.geo.file.shp.ReadShapefile;
-import org.opengis.feature.simple.SimpleFeature;
 
 /**
  * Example: Calculate the total length of all shapefile features.
@@ -23,34 +19,14 @@ public class Completeness {
 
 		// Calculate total length
 		double totalLength = 0;
-		int countedFeatures = 0;
 		try {
-			SimpleFeatureIterator iter = lineFeatures.getFeatures().features();
-			while (iter.hasNext()) {
-				SimpleFeature line = iter.next();
-				Geometry geom = (Geometry) line.getDefaultGeometry();
-				switch (Geometries.get(geom)) {
-				case LINESTRING:
-					totalLength = Length.calculate(geom);
-					countedFeatures++;
-					break;
-				case MULTILINESTRING:
-					for (int idx = 0; idx < geom.getNumGeometries(); idx++) {
-						totalLength = Length.calculate(geom.getGeometryN(idx));
-						countedFeatures++;
-					}
-					break;
-				default:
-					System.out.println("Not supported Geometry! " + line.getType());
-				}
-			}
+			totalLength = Length.calculate(lineFeatures.getFeatures().features());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		// Output result
 		System.out.println("RESULT");
-		System.out.println("Features: " + countedFeatures);
 		System.out.println("Total length [m]: " + totalLength);
 	}
 }
--- src/main/java/org/neis_one/geo/quality/main/PositionalAccuracy.java
+++ src/main/java/org/neis_one/geo/quality/main/PositionalAccuracy.java
@@ -1,5 +1,17 @@
 package org.neis_one.geo.quality.main;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.locationtech.jts.geom.Geometry;
+import org.neis_one.geo.calculation.Transform;
+import org.neis_one.geo.file.shp.ReadShapefile;
+import org.opengis.feature.simple.SimpleFeature;
+
 /**
  * Example: Calculate PositionalAccuracy.
  * 
@@ -9,10 +21,61 @@ public class PositionalAccuracy {
 
 	public static void main(String[] args) {
 		// Read Shapefiles
+		final SimpleFeatureSource lineFeaturesExpected = new ReadShapefile()
+				.get("./src/test/resources/samplelines/SampleLines.shp");
+		final SimpleFeatureSource lineFeaturesActual = new ReadShapefile()
+				.get("./src/test/resources/samplelines/SampleLinesModifiedCopy.shp");
 
-		// Buffer, intersect and calculate length
+		// Transform
+		final Collection<Geometry> geometriesExpected = transform(lineFeaturesExpected);
+		final Collection<Geometry> geometriesActual = transform(lineFeaturesActual);
+		// Buffer
+		final Collection<Geometry> geometriesBuffered = geometriesExpected.stream().map(g -> g.buffer(10))
+				.collect(Collectors.toList());
+		// Intersect
+		final Collection<Geometry> geometriesIntersections = intersect(geometriesActual, geometriesBuffered);
+
+		// Calculate length
+		double expectedLength = calculateLength(geometriesExpected);
+		double actualLength = calculateLength(geometriesActual);
+		double intersectionLength = calculateLength(geometriesIntersections);
 
 		// Output result
+		System.out.println("RESULT");
+		System.out.println("Expected length: " + expectedLength);
+		System.out.println("Expected length: " + actualLength);
+		System.out.println("PositionalAccuracy length[m]: " + intersectionLength);
+	}
 
+	private static Collection<Geometry> intersect(final Collection<Geometry> geometriesActual,
+			final Collection<Geometry> geometriesBuffered) {
+		Collection<Geometry> geometriesIntersections = new ArrayList<>();
+		for (Geometry geom : geometriesActual) {
+			for (Geometry geomBuffer : geometriesBuffered) {
+				Geometry geomIntersection = geom.intersection(geomBuffer);
+				if (!geomIntersection.isEmpty()) {
+					geometriesIntersections.add(geomIntersection);
+				}
+			}
+		}
+		return geometriesIntersections;
+	}
+
+	private static Collection<Geometry> transform(SimpleFeatureSource features) {
+		Collection<Geometry> transformed = new ArrayList<>();
+		try {
+			SimpleFeatureIterator iter = features.getFeatures().features();
+			while (iter.hasNext()) {
+				SimpleFeature line = iter.next();
+				transformed.add(Transform.fromWgs84toUTM32((Geometry) line.getDefaultGeometry()));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return transformed;
+	}
+
+	private static double calculateLength(final Collection<Geometry> geometries) {
+		return geometries.stream().map(g -> g.getLength()).reduce(0.0, Double::sum);
 	}
 }
