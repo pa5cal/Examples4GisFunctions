35b9cc9a19adb2648b074455be9a7bbf9204b741
pa5cal <pascal@neis-one.org>
2019-05-29 14:25:08 +0200

    updated ThematicAccuracyClassificationCorrectness

--- src/main/java/org/neis_one/geo/quality/main/ThematicAccuracyClassificationCorrectness.java
+++ src/main/java/org/neis_one/geo/quality/main/ThematicAccuracyClassificationCorrectness.java
@@ -1,5 +1,14 @@
 package org.neis_one.geo.quality.main;
 
+import java.io.IOException;
+
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.locationtech.jts.geom.Geometry;
+import org.neis_one.geo.calculation.Transform;
+import org.neis_one.geo.file.shp.ReadShapefile;
+import org.opengis.feature.simple.SimpleFeature;
+
 /**
  * Example: Calculate ThematicAccuracy. <br>
  * "Richtigkeit der Klassifikation (classification correctness): Stimmen
@@ -11,11 +20,21 @@ package org.neis_one.geo.quality.main;
  */
 public class ThematicAccuracyClassificationCorrectness {
 
-	public static void main(String[] args) {
-		// #TODO pa5cal implement
+	public static void main(String[] args) throws IOException {
 		// Read Shapefiles
+		final SimpleFeatureSource lineFeaturesExpected = new ReadShapefile()
+				.get("./src/test/resources/samplelines/SampleLines.shp");
+		final SimpleFeatureSource lineFeaturesActual = new ReadShapefile()
+				.get("./src/test/resources/samplelines/SampleLinesModifiedCopy.shp");
 
 		// Buffer, intersect and compare attributes
+		SimpleFeatureIterator iter = lineFeaturesExpected.getFeatures().features();
+		while (iter.hasNext()) {
+			SimpleFeature feature = iter.next();
+			Geometry geom = (Geometry) feature.getDefaultGeometryProperty();
+			// Transform & buffer
+			Geometry bufferedGeom = Transform.fromWgs84toUTM32(geom).buffer(10);
+		}
 
 		// Output result
 
