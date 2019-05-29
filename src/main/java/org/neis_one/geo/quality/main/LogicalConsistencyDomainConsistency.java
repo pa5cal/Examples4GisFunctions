fa9b4b1bdc736d873e92654a4e85545130527af3
pa5cal <pascal@neis-one.org>
2019-05-29 09:06:02 +0200

    new example: LogicalConsistencyDomainConsistency

--- src/main/java/org/neis_one/geo/quality/main/LogicalConsistencyDomainConsistency.java
+++ src/main/java/org/neis_one/geo/quality/main/LogicalConsistencyDomainConsistency.java
@@ -1,10 +1,13 @@
 package org.neis_one.geo.quality.main;
 
 import java.io.IOException;
+import java.util.Set;
 
+import org.apache.commons.compress.utils.Sets;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.neis_one.geo.file.shp.ReadShapefile;
+import org.opengis.feature.simple.SimpleFeature;
 
 /**
  * Example: Check Topological Consistency of all shapefile features.<br>
@@ -18,16 +21,24 @@ public class LogicalConsistencyDomainConsistency {
 
 	public static void main(String[] args) {
 		// Read Shapefile
-		SimpleFeatureSource lineFeatures = new ReadShapefile().get("./src/test/resources/samplelines/SampleLines.shp");
+		SimpleFeatureSource lineFeatures = new ReadShapefile()
+				.get("./src/test/resources/samplelines/SampleLinesModifiedCopy.shp");
 
 		// Check domain consistency
-		int features = 0;
+		int readFeatures = 0;
+		int validFeatures = 0;
 		try {
+			String key = "highway";
+			Set<String> allowedValues = Sets.newHashSet("motorway", "path");
 			SimpleFeatureIterator iter = lineFeatures.getFeatures().features();
 			while (iter.hasNext()) {
-				features++;
-				// SimpleFeature line = iter.next();
-				// Check values ...
+				SimpleFeature line = iter.next();
+				// Check values
+				String value = line.getAttribute(key).toString();
+				if (allowedValues.contains(value)) {
+					validFeatures++;
+				}
+				readFeatures++;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -35,6 +46,7 @@ public class LogicalConsistencyDomainConsistency {
 
 		// Output result
 		System.out.println("RESULT");
-		System.out.println("Features: " + features);
+		System.out.println("Read Features: " + readFeatures);
+		System.out.println("Valid Features: " + validFeatures);
 	}
 }
