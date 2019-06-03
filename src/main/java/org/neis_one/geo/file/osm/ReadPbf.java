5de87101c923820b0e65adec48461cc91d9abfa2
pa5cal <pascal@neis-one.org>
2019-06-03 10:32:37 +0200

    New example: Export highways.

--- src/main/java/org/neis_one/geo/file/osm/ReadPbf.java
+++ src/main/java/org/neis_one/geo/file/osm/ReadPbf.java
@@ -1,22 +1,40 @@
 package org.neis_one.geo.file.osm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.neis_one.geo.filter.attribute.KeyValueFilter;
+import org.neis_one.geo.osm.cache.Store;
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.NodeContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.RelationContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.WayContainer;
-import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
+import org.openstreetmap.osmosis.core.domain.v0_6.EntityType;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
+import org.openstreetmap.osmosis.core.domain.v0_6.Relation;
 import org.openstreetmap.osmosis.core.domain.v0_6.Way;
 import org.openstreetmap.osmosis.core.task.v0_6.Sink;
 
 /**
- * Receives data from the Osmosis pipeline and prints ways which have the
- * 'highway key.
+ * Receives data from the Osmosis pipeline and stores elements based on the
+ * filter.
  * 
  * @author pa5cal
  */
 public class ReadPbf implements Sink {
+
+	private final KeyValueFilter keyValueFilter;
+	private final Store<Node> nodeStore;
+	private final Store<Way> wayStore;
+
+	public ReadPbf(KeyValueFilter keyValueFilter) {
+		this.keyValueFilter = keyValueFilter;
+		nodeStore = new Store<Node>();
+		wayStore = new Store<Way>();
+	}
 
 	@Override
 	public void initialize(Map<String, Object> arg0) {
@@ -25,19 +43,11 @@ public class ReadPbf implements Sink {
 	@Override
 	public void process(EntityContainer entityContainer) {
 		if (entityContainer instanceof NodeContainer) {
-			// Nothing to do here
+			processNode(entityContainer);
 		} else if (entityContainer instanceof WayContainer) {
-			Way myWay = ((WayContainer) entityContainer).getEntity();
-			for (Tag myTag : myWay.getTags()) {
-				if ("highway".equalsIgnoreCase(myTag.getKey())) {
-					System.out.println(" Woha, it's a highway: " + myWay.getId());
-					break;
-				}
-			}
+			processWay(entityContainer);
 		} else if (entityContainer instanceof RelationContainer) {
-			// Nothing to do here
-		} else {
-			System.out.println("Unknown Entity!");
+			processRelation(entityContainer);
 		}
 	}
 
@@ -47,5 +57,89 @@ public class ReadPbf implements Sink {
 
 	@Override
 	public void close() {
+	}
+
+	private void processNode(EntityContainer entityContainer) {
+		nodeStore.saveIfContainsId(((NodeContainer) entityContainer).getEntity());
+	}
+
+	private void processWay(EntityContainer entityContainer) {
+		Way way = ((WayContainer) entityContainer).getEntity();
+		boolean contains = way.getTags().stream()
+				.anyMatch(t -> keyValueFilter.containsIgnoreCase(t.getKey(), t.getValue()));
+		if (contains) {
+			way.getWayNodes().forEach(wn -> nodeStore.putIdToSave(wn.getNodeId()));
+			List<Node> nodes = way.getWayNodes().stream().map(wn -> nodeStore.get(wn.getNodeId()))
+					.filter(n -> n != null).collect(Collectors.toList());
+			if (!nodes.isEmpty()) {
+				// #TODO droneMapElementsResult.add(new DroneMapElement(osmElementFilter, way,
+				// nodes));
+			}
+		}
+		if (wayStore.saveIfContainsId(way)) {
+			way.getWayNodes().forEach(wn -> nodeStore.putIdToSave(wn.getNodeId()));
+		}
+	}
+
+	private void processRelation(EntityContainer entityContainer) {
+		Relation relation = ((RelationContainer) entityContainer).getEntity();
+		boolean contains = relation.getTags().stream()
+				.anyMatch(t -> keyValueFilter.containsIgnoreCase(t.getKey(), t.getValue()));
+		if (contains) {
+			relation.getMembers().stream()
+					.filter(m -> EntityType.Way.compareTo(m.getMemberType()) == 0
+							&& "outer".equalsIgnoreCase(m.getMemberRole()))
+					.forEach(w -> wayStore.putIdToSave(w.getMemberId()));
+			List<Way> ways = relation.getMembers().stream()
+					.filter(m -> EntityType.Way.compareTo(m.getMemberType()) == 0
+							&& "outer".equalsIgnoreCase(m.getMemberRole()))
+					.map(w -> wayStore.get(w.getMemberId())).filter(w -> w != null).collect(Collectors.toList());
+			if (!ways.isEmpty()) {
+				List<List<Node>> nodeLists = new ArrayList<>();
+				for (Way w : ways) {
+					List<Node> nodeList = w.getWayNodes().stream().map(wn -> nodeStore.get(wn.getNodeId()))
+							.filter(n -> n != null).collect(Collectors.toList());
+					if (!nodeList.isEmpty()) {
+						nodeLists.add(nodeList);
+					}
+				}
+				if (!nodeLists.isEmpty()) {
+					List<Node> nodes = new ArrayList<Node>();
+					nodes.addAll(nodeLists.get(0));
+					nodeLists.remove(0);
+					for (int idx = 0; idx < nodeLists.size(); idx++) {
+						List<Node> n = nodeLists.get(idx);
+						if (merge(nodes, n) || reverseMerge(nodes, n)) {
+							nodeLists.remove(idx);
+							idx = -1;
+						}
+					}
+					if (!nodes.isEmpty()) {
+						// #TODO droneMapElementsResult.add(new DroneMapElement(osmElementFilter,
+						// relation, nodes));
+					}
+				}
+			}
+		}
+	}
+
+	private boolean merge(List<Node> nodes, List<Node> n) {
+		boolean merged = false;
+		if (nodes.get(0).equals(n.get(0))) {
+			nodes.remove(0);
+			Collections.reverse(nodes);
+			nodes.addAll(n);
+			merged = true;
+		} else if (nodes.get(nodes.size() - 1).equals(n.get(0))) {
+			n.remove(0);
+			nodes.addAll(n);
+			merged = true;
+		}
+		return merged;
+	}
+
+	private boolean reverseMerge(List<Node> nodes, List<Node> n) {
+		Collections.reverse(n);
+		return merge(nodes, n);
 	}
 }
---
+++ src/main/java/org/neis_one/geo/filter/attribute/KeyValueFilter.java
@@ -0,0 +1,33 @@
+package org.neis_one.geo.filter.attribute;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class KeyValueFilter {
+
+	private final Map<String, Set<String>> expectedLowerKeyValues;
+
+	public KeyValueFilter(Map<String, Set<String>> expectedKeyValues) {
+		expectedLowerKeyValues = new HashMap<>();
+		// Convert input map to lower case
+		for (Map.Entry<String, Set<String>> entry : expectedKeyValues.entrySet()) {
+			expectedLowerKeyValues.put(entry.getKey().toLowerCase(),
+					entry.getValue().stream().map(v -> v.toLowerCase()).collect(Collectors.toSet()));
+		}
+	}
+
+	public boolean containsIgnoreCase(String key, String value) {
+		boolean contains = false;
+		String lowerKey = key.toLowerCase();
+		String lowerValue = value.toLowerCase();
+
+		Set<String> values = expectedLowerKeyValues.get(lowerKey);
+		if (Objects.nonNull(values)) {
+			contains = values.contains(lowerValue);
+		}
+		return contains;
+	}
+}
