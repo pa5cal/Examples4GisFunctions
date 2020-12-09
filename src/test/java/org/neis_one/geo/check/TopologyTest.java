8fb36aa14eb9c517c6b9cad13f73133cfb550f70
pa5cal <pascal@neis-one.org>
2020-12-09 13:30:25 +0100

    Add license header.

--- src/test/java/org/neis_one/geo/check/TopologyTest.java
+++ src/test/java/org/neis_one/geo/check/TopologyTest.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 Pascal Neis <neis-one.org>.
+
+This file is part of Examples4GisFunctions (https://github.com/pa5cal)
+
+Examples4GisFunctions is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+Examples4GisFunctions is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.neis_one.geo.check;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
